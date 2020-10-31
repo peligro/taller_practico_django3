79e0a40e401c02d11b1863b3095552c478812824
peligro <yo@cesarcancino.com>
2020-10-31 15:34:52 -0300

    videotutorial 6

--- taller_practico/home/models.py
+++ taller_practico/home/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+
 # Create your models here.
+
+"""
+create table categorias(
+    id int not null auto_increment primary key,
+    nombre varchar(100),
+    slug varchar(100)
+)
+"""
+
+
+class Categorias(models.Model):
+    
+    nombre = models.CharField(max_length=100, null=True, default='vacio')
+    slug = models.CharField(max_length=100, null=True, default='vacio')
+    
+    
+    class Meta:
+        managed = True
+        db_table = 'categorias'
+
+
+class Post(models.Model):
+    categorias = models.ForeignKey(Categorias, models.DO_NOTHING, null=True)
+    nombre = models.CharField(max_length=100, null=True, default='vacio')
+    slug = models.CharField(max_length=100, null=True, default='vacio')
+    detalle = models.TextField(null=True)
+    fecha  = models.DateTimeField()
+    
+    class Meta:
+        managed = True
+        db_table = 'post'
+    
+
+
+
+    
--- taller_practico/taller_practico/settings.py
+++ taller_practico/taller_practico/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rutas'
+    'rutas',
+    'home'
 ]
 
 MIDDLEWARE = [
